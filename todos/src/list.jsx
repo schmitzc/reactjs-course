fe9319dc8ea559ff70eafd58d90ccf2a8bd0300b
Cassie Schmitz <schmitzc@gmail.com>
2015-12-23 09:54:27 -0800

    Fix displaying of empty list message

--- todos/src/list.jsx
+++ todos/src/list.jsx
@@ -8,7 +8,7 @@ module.exports = React.createClass({
   },
 
   renderList: function() {
-    if (this.props.items && Object.keys(this.props.items).length === 0) {
+    if (!this.props.items || Object.keys(this.props.items).length === 0) {
       return <h4>Add a task to get started.</h4>
     } else {
       var items = [];
