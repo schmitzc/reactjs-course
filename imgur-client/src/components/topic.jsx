1c170b58b2ee7e54acda33854861f8fa19c6acc9
Cassie Schmitz <schmitzc@gmail.com>
2015-12-29 08:23:21 -0800

    Show images in topic

--- imgur-client/src/components/topic.jsx
+++ imgur-client/src/components/topic.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var Reflux = require('reflux');
 var Actions = require('../actions');
 var ImageStore = require('../stores/image-store');
+var ImagePreview = require('./image-preview');
 
 module.exports = React.createClass({
   mixins: [
@@ -24,8 +25,14 @@ module.exports = React.createClass({
 
   render: function() {
     return <div>
-      I am a topic #{this.props.params.id}!
+      {this.renderImages()}
     </div>
+  },
+
+  renderImages: function() {
+    return this.state.images.slice(0, 20).map(function(image) {
+      return <ImagePreview key={image.id} {...image} />
+    });
   },
 
   onChange: function(event, images) {
