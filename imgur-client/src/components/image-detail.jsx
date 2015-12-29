9fdd288410a4f2c49fb84e46f4220916f8e5ea25
Cassie Schmitz <schmitzc@gmail.com>
2015-12-29 09:39:12 -0800

    Fetch image data when viewing image detail

--- imgur-client/src/components/image-detail.jsx
+++ imgur-client/src/components/image-detail.jsx
@@ -1,9 +1,32 @@
 var React = require('react');
+var Reflux = require('reflux');
+var Actions = require('../actions')
+var ImageStore = require('../stores/image-store');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
+
+  getInitialState: function() {
+    return {
+      image: null
+    };
+  },
+
+  componentWillMount: function() {
+    Actions.getImage(this.props.params.id);
+  },
+
   render: function() {
     return <div>
-      I am an image detail
+      {this.state.image}
     </div>;
+  },
+
+  onChange: function(event, image) {
+    this.setState({
+      image: ImageStore.find(this.props.params.id)
+    });
   }
 });
--- imgur-client/src/stores/image-store.jsx
+++ imgur-client/src/stores/image-store.jsx
@@ -17,6 +17,27 @@ module.exports = Reflux.createStore({
       }.bind(this));
   },
 
+  getImage: function(id) {
+    Api.get('image/' + id)
+      .then(function(json) {
+        if (this.images) {
+          this.images.push(json.data);
+        } else {
+          this.images = [json.data];
+        }
+
+        this.triggerChange();
+      }.bind(this));
+  },
+
+  find: function(id) {
+    var image = _.findWhere(this.images, {id: id});
+    if (image) return image;
+
+    this.getImage(id);
+    return null;
+  },
+
   triggerChange: function() {
     this.trigger('change', this.images);
   }
