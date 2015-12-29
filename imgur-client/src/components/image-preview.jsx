0b2549289947113aa187907787e1fb378bc9b75d
Cassie Schmitz <schmitzc@gmail.com>
2015-12-29 09:18:42 -0800

    Link image to image detail page

--- imgur-client/src/components/image-preview.jsx
+++ imgur-client/src/components/image-preview.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 module.exports = React.createClass({
   getInitialState: function() {
@@ -8,7 +10,8 @@ module.exports = React.createClass({
   },
 
   render: function() {
-    return <div
+    return <Link
+      to={'images/'  + this.props.id}
       className='image-preview'
       onMouseEnter={this.handleMouseEnter}
       onMouseLeave={this.handleMouseLeave}>
@@ -18,7 +21,7 @@ module.exports = React.createClass({
       {this.props.animated && !this.state.hovering ? this.icon() : null }
 
       {this.state.hovering ? this.inset() : null }
-    </div>
+    </Link>
   },
 
   image: function() {
