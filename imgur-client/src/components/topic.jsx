26b245cd7031e8a64fdf5d237460e117cdbd064c
Cassie Schmitz <schmitzc@gmail.com>
2015-12-28 17:58:21 -0800

    Get list of images for topic

--- imgur-client/src/components/topic.jsx
+++ imgur-client/src/components/topic.jsx
@@ -1,9 +1,30 @@
 var React = require('react');
+var Reflux = require('reflux');
+var Actions = require('../actions');
+var ImageStore = require('../stores/image-store');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
+
+  getInitialState: function() {
+    return {
+      images: []
+    };
+  },
+
+  componentWillMount: function() {
+    Actions.getImages(this.props.params.id);
+  },
+
   render: function() {
     return <div>
-      I am a topic #{this.props.id}!
+      I am a topic #{this.props.params.id}!
     </div>
+  },
+
+  onChange: function(event, images) {
+    this.setState({images: images});
   }
 });
