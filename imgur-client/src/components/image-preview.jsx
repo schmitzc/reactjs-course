cff40a57c1c6c6a4c7011206730027befcb6cda1
Cassie Schmitz <schmitzc@gmail.com>
2015-12-29 08:59:13 -0800

    Play video on mouse hover

--- imgur-client/src/components/image-preview.jsx
+++ imgur-client/src/components/image-preview.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      hovering: false
+    };
+  },
+
   render: function() {
-    return <div className='image-preview'>
-      {this.image()}
+    return <div
+      className='image-preview'
+      onMouseEnter={this.handleMouseEnter}
+      onMouseLeave={this.handleMouseLeave}>
+
+      {this.props.animated && this.state.hovering ? this.video() : this.image()}
+
+      {this.props.animated && !this.state.hovering ? this.icon() : null }
     </div>
   },
 
@@ -11,5 +23,25 @@ module.exports = React.createClass({
     var link = 'http://i.imgur.com/' + this.props.id + 'h.jpg';
 
     return <img src={link} />
+  },
+
+  video: function() {
+    return <div>
+      <video preload='auto' autoPlay='autoplay' loop='loop' webkit-playsinline>
+        <source src={this.props.mp4} type='video/mp4' />
+      </video>
+    </div>;
+  },
+
+  icon: function() {
+    return <span className='glyphicon glyphicon-play'></span>
+  },
+
+  handleMouseEnter: function() {
+    this.setState({hovering: true});
+  },
+
+  handleMouseLeave: function() {
+    this.setState({hovering: false});
   }
 });
